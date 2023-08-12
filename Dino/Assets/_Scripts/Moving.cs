8bc88e0b777113b8790685f82c83152fc213fd2b
KatyaZav <Party2015ru@mail.ru>
2023-08-12 21:09:11 +0500

    infinity running walls

--- Dino/Assets/_Scripts/Moving.cs
+++ Dino/Assets/_Scripts/Moving.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
+    Spawner spawner;
     Rigidbody2D rb;
+    bool isSpawned = false;
+
     private void Start()
     {
+        spawner = FindObjectOfType<Spawner>();
         rb = GetComponent<Rigidbody2D>();
     }
     
-    void FixedUpdate()
+    void Update()
     {
         int speed = 2;
-        rb.velocity = new Vector2(-speed, 0);    
+        rb.velocity = new Vector2(-speed, 0);
+
+        if (transform.position.x <= -25)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.x <= 0.5 && !isSpawned)
+        {
+            spawner.CreateLvl();
+            isSpawned = true;
+        }
     }
 }
---
+++ Dino/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject[] lvls;
+
+    private void Awake()
+    {
+        //CreateLvl();
+    }
+    public void CreateLvl()
+    {
+        var rand = Random.Range(0, lvls.Length);
+        Instantiate(lvls[rand], new Vector2(spawnPoint.transform.position.x, spawnPoint.transform.position.y), Quaternion.identity);
+    }    
+}
