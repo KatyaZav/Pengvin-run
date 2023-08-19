57031e614d883f1f4ffd2651c8ee40c36d87c1d9
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2023-08-19 22:39:52 +0500

    v2

---
+++ Dino/Assets/_Scripts/Player/Effects.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : MonoBehaviour
+{
+    [SerializeField] GameObject[] jumpEffects;
+    [SerializeField] GameObject[] diedEffects;
+}
